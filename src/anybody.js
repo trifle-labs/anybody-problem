6fa4b95fd24822b5892e83487020a1b1e03e3bff
psugihara <peter@campsh.com>
2024-04-11 08:44:34 -0700

    rm debug setup

--- src/anybody.js
+++ src/anybody.js
@@ -5,7 +5,7 @@ import Sound from './sound.js'
 import { Visuals, FPS } from './visuals.js'
 import { _validateSeed, Calculations } from './calculations.js'
 
-const GAME_LENGTH = 2 // seconds
+const GAME_LENGTH = 60 // seconds
 
 export class Anybody extends EventEmitter {
   constructor(p, options = {}) {
@@ -248,7 +248,7 @@ export class Anybody extends EventEmitter {
     this.witherAllBodies()
     this.sound?.playGameOver({ won })
     this.gameOver = true
-    this.won = true
+    this.won = won
     if (this.won) {
       void this.setStatsText()
     } else {
