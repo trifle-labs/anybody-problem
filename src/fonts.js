157a8cb4cce7756b780f8bf1e3b4bf7d92035463
psugihara <peter@campsh.com>
2024-06-26 16:55:55 -0700

    inline fonts

--- src/fonts.js
+++ src/fonts.js
@@ -1,13 +1,5 @@
-const bodyFontURL = new URL(
-  '/public/fonts/Space-Notorious-rounded.otf',
-  import.meta.url
-).href
-
-// n.b. to make this font load, I had to remove the leading numbers from the filename
-const dotFontURL = new URL(
-  '/public/fonts/A000-Dots-edited.ttf',
-  import.meta.url
-).href
+import bodyFontURL from 'data-url:../public/fonts/Space-Notorious-rounded.otf'
+import dotFontURL from 'data-url:../public/fonts/A000-Dots-edited.ttf'
 
 export const fonts = { body: null, dot: null }
 
@@ -23,6 +15,7 @@ export function loadFonts(p) {
     }
     p.loadFont(
       url,
+      fontName,
       (font) => {
         fonts[fontName] = font
       },
